7b942d9801fc3831439d542c2365b15963f715a9
Bright Dikko <BDikko@nd.edu>
2024-01-04 16:40:52 -0500

    [IMPLEMENTATION] Create and add table for favorites listings to the data base

--- purrchaser-backend/src/main/resources/db/migration/V1__init_db.sql
+++ purrchaser-backend/src/main/resources/db/migration/V1__init_db.sql
@@ -133,3 +133,12 @@ CREATE TABLE listing_tags
     FOREIGN KEY (listing_id) REFERENCES listings (listing_id),
     FOREIGN KEY (tag_id) REFERENCES tags (tag_id)
 );
+
+CREATE TABLE favorite_listings (
+    favorite_listing_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    listing_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users (user_id),
+    FOREIGN KEY (listing_id) REFERENCES listings (listing_id)
+);
