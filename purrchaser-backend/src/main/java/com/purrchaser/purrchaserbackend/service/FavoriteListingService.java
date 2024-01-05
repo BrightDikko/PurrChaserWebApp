c3dab86741982199a51425abf954a8d1af6a0015
Bright Dikko <BDikko@nd.edu>
2024-01-05 16:49:52 -0500

    [IMPLEMENTATION] Implement service to handle the adding of an item to user's favorites

--- purrchaser-backend/src/main/java/com/purrchaser/purrchaserbackend/service/FavoriteListingService.java
+++ purrchaser-backend/src/main/java/com/purrchaser/purrchaserbackend/service/FavoriteListingService.java
@@ -1,2 +1,10 @@
-package com.purrchaser.purrchaserbackend.service;public interface FavoriteListingService {
+package com.purrchaser.purrchaserbackend.service;
+
+import com.purrchaser.purrchaserbackend.dto.FavoriteListingDTO;
+import com.purrchaser.purrchaserbackend.response.GenericApplicationResponse;
+
+public interface FavoriteListingService {
+
+    GenericApplicationResponse<FavoriteListingDTO> addListingToFavorites(Integer userId, Integer listingId);
+
 }
