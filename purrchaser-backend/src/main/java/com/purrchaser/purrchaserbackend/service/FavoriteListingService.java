2fd296c57d4a18489489a30ef044c1774706cb26
Bright Dikko <BDikko@nd.edu>
2024-01-05 22:14:42 -0500

    [IMPLEMENTATION] Implement service functionality for removing a favorited listing from user's favorite and overload ApplicationResponseBuilder's buildResponse method to return appropriate response for the functionality

--- purrchaser-backend/src/main/java/com/purrchaser/purrchaserbackend/service/FavoriteListingService.java
+++ purrchaser-backend/src/main/java/com/purrchaser/purrchaserbackend/service/FavoriteListingService.java
@@ -6,5 +6,6 @@ import com.purrchaser.purrchaserbackend.response.GenericApplicationResponse;
 public interface FavoriteListingService {
 
     GenericApplicationResponse<FavoriteListingDTO> addListingToFavorites(Integer userId, Integer listingId);
+    GenericApplicationResponse<Void> removeListingFromFavorites(Integer userId, Integer listingId);
 
 }
