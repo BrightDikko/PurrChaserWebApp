e33c39ccecbdc760913a571264b88a1e842be121
Bright Dikko <BDikko@nd.edu>
2024-01-08 23:12:43 -0500

    [IMPLEMENTATION] Create cart controller for adding an item to user's cart, and allow-list endpoint

--- purrchaser-backend/src/main/java/com/purrchaser/purrchaserbackend/controller/CartController.java
+++ purrchaser-backend/src/main/java/com/purrchaser/purrchaserbackend/controller/CartController.java
@@ -1,2 +1,31 @@
-package com.purrchaser.purrchaserbackend.controller;public class CartController {
+package com.purrchaser.purrchaserbackend.controller;
+
+import com.purrchaser.purrchaserbackend.dto.CartDTO;
+import com.purrchaser.purrchaserbackend.response.GenericApplicationResponse;
+import com.purrchaser.purrchaserbackend.service.CartService;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cart")
+@RequiredArgsConstructor
+public class CartController {
+
+    private final CartService cartService;
+
+    @PostMapping("/user/{userId}/listing/{listingId}")
+    public ResponseEntity<GenericApplicationResponse<CartDTO>> addListingToUserCart(
+            @PathVariable @NotNull Integer userId,
+            @PathVariable @NotNull Integer listingId
+    ) {
+
+        GenericApplicationResponse<CartDTO> response = cartService.addListingToUserCart(userId, listingId);
+        return ResponseEntity.ok(response);
+
+    }
 }
