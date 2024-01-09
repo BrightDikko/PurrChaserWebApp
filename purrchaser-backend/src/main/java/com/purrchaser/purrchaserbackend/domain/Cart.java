7f236f1bceb3ebf99eac76898e6cddfd3843be6b
Bright Dikko <BDikko@nd.edu>
2024-01-08 23:13:25 -0500

    [IMPLEMENTATION] Implement cart domain, dto, and mapper

--- purrchaser-backend/src/main/java/com/purrchaser/purrchaserbackend/domain/Cart.java
+++ purrchaser-backend/src/main/java/com/purrchaser/purrchaserbackend/domain/Cart.java
@@ -1,2 +1,57 @@
-package com.purrchaser.purrchaserbackend.domain;public class Cart {
+package com.purrchaser.purrchaserbackend.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+import java.util.Objects;
+
+@Entity
+@Table(name = "carts")
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cart {
+
+    @Id
+    @Column(columnDefinition = "CHAR(36)")
+    private String cartId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private ApplicationUser user;
+
+    @ManyToOne
+    @JoinColumn(name = "listing_id", nullable = false)
+    private Listing listing;
+
+    @Column(name = "quantity", nullable = false)
+    private Integer quantity;
+
+    @Column(nullable = false, name = "added_at")
+    private Timestamp addedAt;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cart that = (Cart) o;
+        return Objects.equals(this.cartId, that.cartId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.cartId);
+    }
 }
--- purrchaser-backend/src/main/java/com/purrchaser/purrchaserbackend/mapper/CartMapper.java
+++ purrchaser-backend/src/main/java/com/purrchaser/purrchaserbackend/mapper/CartMapper.java
@@ -1,2 +1,20 @@
-package com.purrchaser.purrchaserbackend.mapper;public class CartMapper {
+package com.purrchaser.purrchaserbackend.mapper;
+
+import com.purrchaser.purrchaserbackend.domain.Cart;
+import com.purrchaser.purrchaserbackend.dto.CartDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CartMapper {
+    public final ListingMapper listingMapper;
+
+    public CartDTO convertToCartDTO(Cart cart) {
+        return CartDTO.builder()
+                .listing(listingMapper.convertToListingDTO(cart.getListing()))
+                .quantity(cart.getQuantity())
+                .addedAt(cart.getAddedAt())
+                .build();
+    }
 }
