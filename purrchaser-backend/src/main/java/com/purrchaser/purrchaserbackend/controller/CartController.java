1b2f418e05ba3c830e791379e7c8fa3a19357833
Bright Dikko <BDikko@nd.edu>
2024-01-14 14:34:26 -0500

    [IMPLEMENTAION] Implement DELETE mapping for removing a listing item from user's cart, and expose endpoint

--- purrchaser-backend/src/main/java/com/purrchaser/purrchaserbackend/controller/CartController.java
+++ purrchaser-backend/src/main/java/com/purrchaser/purrchaserbackend/controller/CartController.java
@@ -6,6 +6,7 @@ import com.purrchaser.purrchaserbackend.service.CartService;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,5 +28,15 @@ public class CartController {
         GenericApplicationResponse<CartDTO> response = cartService.addListingToUserCart(userId, listingId);
         return ResponseEntity.ok(response);
 
+    }
+
+    @DeleteMapping("/user/{userId}/listing/{listingId}")
+    public ResponseEntity<GenericApplicationResponse<Void>> removeListingFromUserCart(
+            @PathVariable @NotNull Integer userId,
+            @PathVariable @NotNull Integer listingId
+    ) {
+
+        GenericApplicationResponse<Void> response = cartService.removeListingFromUserCart(userId, listingId);
+        return ResponseEntity.ok(response);
     }
 }
