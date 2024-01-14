112166c52823b7068f226940b873c0b27cee4eb6
Bright Dikko <BDikko@nd.edu>
2024-01-14 16:31:37 -0500

    [IMPLEMENTAION] Implement GET mapping for retrieving all listing items from user's cart, and expose endpoint

--- purrchaser-backend/src/main/java/com/purrchaser/purrchaserbackend/controller/CartController.java
+++ purrchaser-backend/src/main/java/com/purrchaser/purrchaserbackend/controller/CartController.java
@@ -7,11 +7,16 @@ import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+import static com.purrchaser.purrchaserbackend.constants.PathConstants.ALL;
 import static com.purrchaser.purrchaserbackend.constants.PathConstants.CART;
 import static com.purrchaser.purrchaserbackend.constants.PathConstants.USER_ID_AND_LISTING_ID_PATH_VARIABLES;
 
@@ -41,5 +46,14 @@ public class CartController {
 
         GenericApplicationResponse<Void> response = cartService.removeListingFromUserCart(userId, listingId);
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping(ALL)
+    public ResponseEntity<GenericApplicationResponse<List<CartDTO>>> getAllListingsInUserCart(
+            @RequestParam @NotNull Integer userId) {
+
+        GenericApplicationResponse<List<CartDTO>> response = cartService.getAllListingsInUserCart(userId);
+        return ResponseEntity.ok(response);
+
     }
 }
